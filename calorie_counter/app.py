656b42e1b2313b7d8bc4fae7c51bf421ab19c1a2
i2nerd4u <karl360@live.ie>
2025-11-15 12:51:50 +0000

    test4

--- calorie_counter/app.py
+++ calorie_counter/app.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             counter.reset_day()
             print("Day reset.")
         elif choice == "4":
-            print("Exiting application, goodbye! see ya later -Karl McDonnell")
+            print("Exiting application, goodbye! -Karl McDonnell")
             break
